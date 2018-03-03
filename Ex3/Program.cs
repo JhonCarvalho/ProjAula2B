21855892acd83becc39d7f4cdfe09bca6c17011d
bacs93 <aluno@fiar.local>
2018-03-02 21:49:57 -0300

    Ex 3,4 e 5

--- Ex3/Program.cs
+++ Ex3/Program.cs
@@ -10,6 +10,25 @@ namespace Ex3
     {
         static void Main(string[] args)
         {
+            /*
+               O custo de um carro novo ao consumidor é a soma do custo de fábrica com a porcentagem
+               do distribuidor e dos impostos (aplicados ao custo de fábrica). Supondo que o percentual do
+               distribuidor seja de 28% e os impostos de 45%, escrever um algoritmo para ler o custo de
+               fábrica de um carro, calcular e escrever o custo final ao consumidor.
+            */
+
+            double custo_fabrica,distribuidor, imposto , custo_final;
+            Console.WriteLine("Informe o valor do custo de fabrica do carro: R$");
+            custo_fabrica =Convert.ToDouble(Console.ReadLine());
+
+            distribuidor =0.28;
+            imposto = 0.45;
+
+            custo_final = custo_fabrica + (custo_fabrica * distribuidor) + (custo_fabrica * imposto);
+
+            Console.WriteLine("O custo final do é : R$" + custo_final);
+            Console.ReadKey();
+                                    
         }
     }
 }
---
+++ Ex4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+              Uma revendedora de carros usados paga a seus funcionários vendedores um salário fixo por
+              mês, mais uma comissão também fixa para cada carro vendido e mais 5% do valor das
+              vendas por ele efetuadas. Escrever um algoritmo que leia o número de carros por ele
+              vendidos, o valor total de suas vendas, o salário fixo e o valor que ele recebe por carro
+              vendido. Calcule e escreva o salário final do vendedor.*/
+
+            int qtd_carro;
+            double salario_fixo,por_carro,valor_venda, salario_final;
+
+            Console.WriteLine("Informe a quantidade de carros vendidos: ");
+            qtd_carro =Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Informe o valor total da venda: ");
+            valor_venda =Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Informe o valor do salário fixo: ");
+            salario_fixo = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Informe o valor que o funcionario reberá por carro vendido : ");
+            por_carro = Convert.ToDouble(Console.ReadLine());
+
+            salario_final = salario_fixo + (qtd_carro * por_carro) + (valor_venda * 0.05);
+
+            Console.WriteLine("O salario final do vendedor é de : R$" + salario_final);
+
+            Console.ReadKey();
+
+        }
+    }
+}
